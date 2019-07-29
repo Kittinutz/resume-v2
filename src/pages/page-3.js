107b2458ca503438fc9537e408606df9dbbd01bc
Kittinutz <knightza94@gmail.com>
2019-07-29 22:55:26 +0700

    fix ignore mongo

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -5,23 +5,23 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => {
-  const data = useStaticQuery(graphql`
-  query {
-    mongodbProfileBio {
-      id
-      name
-      surname
-    }
-  }
-`)
+  //   const data = useStaticQuery(graphql`
+  //   query {
+  //     mongodbProfileBio {
+  //       id
+  //       name
+  //       surname
+  //     }
+  //   }
+  // `)
   return (
-  <Layout>
-    <SEO title="Page two" />
-    {data.mongodbProfileBio.name}
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+      <SEO title="Page two" />
+      {/* {data.mongodbProfileBio.name} */}
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
   )
 
 }
