9d8fd0722c1ef896a1e463a241e71851267c8bd6
Kittinutz <knightza94@gmail.com>
2019-08-06 17:28:01 +0700

    add plugin offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,43 @@ module.exports = {
       options: {
       },
     },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        importWorkboxFrom: `local`,
+        globDirectory: rootDir,
+        globPatterns,
+        modifyUrlPrefix: {
+          // If `pathPrefix` is configured by user, we should replace
+          // the default prefix with `pathPrefix`.
+          "/": `${pathPrefix}/`,
+        },
+        cacheId: `gatsby-plugin-offline`,
+        // Don't cache-bust JS or CSS files, and anything in the static directory,
+        // since these files have unique URLs and their contents will never change
+        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`,
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`,
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `staleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+      }
+    },
     // {
     //   resolve: `gatsby-source-mongodb`,
     //   options: {
@@ -41,7 +78,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/resume-v2`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
